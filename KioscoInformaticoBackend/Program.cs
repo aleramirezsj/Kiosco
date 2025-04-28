15f278720c34a2fdf67f5174dc011d498f69c659
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-04-28 16:02:03 -0300

    Actualizar configuración de CORS en Program.cs
    Se ha cambiado la política de CORS de "AllowAll" a "AllowSpecificOrigins", permitiendo solo orígenes específicos: "https://kioscoale.azurewebsites.net/", "https://www.kioscoale.azurewebsites.net/" y "https://localhost:7190". También se ha actualizado el uso de esta política en la aplicación.

--- KioscoInformaticoBackend/Program.cs
+++ KioscoInformaticoBackend/Program.cs
@@ -38,11 +38,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Configurar una política de CORS
+// Configurar una política de CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
+    options.AddPolicy("AllowSpecificOrigins",
         builder => builder
-            .AllowAnyOrigin()
+            .WithOrigins("https://kioscoale.azurewebsites.net/",
+                    "https://www.kioscoale.azurewebsites.net/",
+                    "https://localhost:7190")
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
@@ -56,7 +59,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors("AllowAll");
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
