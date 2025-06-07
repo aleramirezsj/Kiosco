9e73048f7658ce3e2d801fc62a9c04f38fd953b4
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-06-07 10:27:27 -0300

    Apply state pattern to Localidades form

--- KioscoInformaticoDesktop/Views/LocalidadesView.cs
+++ KioscoInformaticoDesktop/Views/LocalidadesView.cs
@@ -19,10 +19,19 @@ namespace KioscoInformaticoDesktop.Views
         BindingSource listaLocalidades = new BindingSource();
         Localidad localidadCurrent;
 
+        State _state;
+
+        private async Task SetStateAsync(State state)
+        {
+            _state = state;
+            await state.Enter();
+        }
+
         public LocalidadesView()
         {
             InitializeComponent();
             dataGridLocalidades.DataSource = listaLocalidades;
+            _state = new ListState(this);
             CargarGrilla();
         }
 
@@ -31,11 +40,10 @@ namespace KioscoInformaticoDesktop.Views
             listaLocalidades.DataSource = await localidadService.GetAllAsync();
         }
 
-        private void btnAgregar_Click(object sender, EventArgs e)
+        private async void btnAgregar_Click(object sender, EventArgs e)
         {
-            tabControl.SelectTab(tabPageAgregarEditar);
+            await SetStateAsync(new AddState(this));
         }
-
         private async void btnGuardar_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(txtNombre.Text))
@@ -43,33 +51,20 @@ namespace KioscoInformaticoDesktop.Views
                 MessageBox.Show("El nombre de la localidad es obligatorio", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
-            if (localidadCurrent != null)
+            if (_state != null)
             {
-                localidadCurrent.Nombre = txtNombre.Text;
-                await localidadService.UpdateAsync(localidadCurrent);
-                localidadCurrent = null;
+                await _state.SaveAsync();
             }
-            else
-            {
-                var localidad = new Localidad
-                {
-                    Nombre = txtNombre.Text
-                };
-                await localidadService.AddAsync(localidad);
-            }
-            await CargarGrilla();
-            txtNombre.Text = string.Empty;
-            tabControl.SelectTab(tabPageLista);
         }
 
-        private void btnModificar_Click(object sender, EventArgs e)
+        private async void btnModificar_Click(object sender, EventArgs e)
         {
-            localidadCurrent = (Localidad)listaLocalidades.Current;
-            txtNombre.Text = localidadCurrent.Nombre;
-            tabControl.SelectTab(tabPageAgregarEditar);
+            var localidad = (Localidad)listaLocalidades.Current;
+            if (localidad != null)
+            {
+                await SetStateAsync(new EditState(this, localidad));
+            }
         }
-
         private async void btnEliminar_Click(object sender, EventArgs e)
         {
             localidadCurrent = (Localidad)listaLocalidades.Current;
@@ -82,14 +77,13 @@ namespace KioscoInformaticoDesktop.Views
             localidadCurrent = null;
         }
 
-        private void btnCancelar_Click(object sender, EventArgs e)
+        private async void btnCancelar_Click(object sender, EventArgs e)
         {
-            localidadCurrent = null;
-            txtNombre.Text = string.Empty;
-            tabControl.SelectTab(tabPageLista);
-
+            if (_state != null)
+            {
+                await _state.CancelAsync();
+            }
         }
-
         private void BtnBuscar_Click(object sender, EventArgs e)
         {
             FiltrarLocalidades();
@@ -102,6 +96,78 @@ namespace KioscoInformaticoDesktop.Views
 
         private void txtFiltro_TextChanged(object sender, EventArgs e)
         {
+        }
+
+        private abstract class State
+        {
+            protected LocalidadesView view;
+            protected State(LocalidadesView view) { this.view = view; }
+            public virtual Task Enter() { return Task.CompletedTask; }
+            public virtual Task SaveAsync() { return Task.CompletedTask; }
+            public virtual Task CancelAsync() { return Task.CompletedTask; }
+        }
+
+        private class ListState : State
+        {
+            public ListState(LocalidadesView view) : base(view) { }
+            public override async Task Enter()
+            {
+                await view.CargarGrilla();
+                view.txtNombre.Text = string.Empty;
+                view.localidadCurrent = null;
+                view.tabControl.SelectTab(view.tabPageLista);
+            }
+        }
+
+        private class AddState : State
+        {
+            public AddState(LocalidadesView view) : base(view) { }
+            public override Task Enter()
+            {
+                view.txtNombre.Text = string.Empty;
+                view.localidadCurrent = null;
+                view.tabControl.SelectTab(view.tabPageAgregarEditar);
+                return Task.CompletedTask;
+            }
+            public override async Task SaveAsync()
+            {
+                var localidad = new Localidad
+                {
+                    Nombre = view.txtNombre.Text
+                };
+                await view.localidadService.AddAsync(localidad);
+                await view.SetStateAsync(new ListState(view));
+            }
+            public override async Task CancelAsync()
+            {
+                await view.SetStateAsync(new ListState(view));
+            }
+        }
+
+        private class EditState : State
+        {
+            public EditState(LocalidadesView view, Localidad localidad) : base(view)
+            {
+                view.localidadCurrent = localidad;
+            }
+            public override Task Enter()
+            {
+                view.txtNombre.Text = view.localidadCurrent.Nombre;
+                view.tabControl.SelectTab(view.tabPageAgregarEditar);
+                return Task.CompletedTask;
+            }
+            public override async Task SaveAsync()
+            {
+                view.localidadCurrent.Nombre = view.txtNombre.Text;
+                await view.localidadService.UpdateAsync(view.localidadCurrent);
+                view.localidadCurrent = null;
+                await view.SetStateAsync(new ListState(view));
+            }
+            public override async Task CancelAsync()
+            {
+                view.localidadCurrent = null;
+                await view.SetStateAsync(new ListState(view));
+            }
         }
     }
 }
